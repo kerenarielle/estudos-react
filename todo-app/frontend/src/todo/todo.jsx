6e26ffd036a3ad01c2f1b7415c7a1f8cd9cc7d5c
Keren Leite - Genial Investimentos <keren.leite@genialinvestimentos.com.br>
2019-05-10 18:23:33 -0300

    Estudos react

---
+++ todo-app/frontend/src/todo/todo.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import PageHeader from '../template/pageHeader'
+import TodoForm from './todoForm'
+import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
+
+export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: []}
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description }).then(resp => console.log('funcionou!!!'))
+    }
+
+    handleChange(e) {
+        this.setState({
+            ...this.state, description: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm description={this.state.description} handleAdd={this.handleAdd} handleChange={this.handleChange}></TodoForm>
+                <TodoList></TodoList>
+            </div>
+        )
+    }
+}
---
+++ todo-app/frontend/src/todo/todoList.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default props => (
+    <ul>
+        <li>1</li>
+    </ul>
+)
